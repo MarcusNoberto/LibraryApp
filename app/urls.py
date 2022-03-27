92096340c1d3141d7a835e30f12d2a4d5d2a20b2
MarcusNoberto <marcus.ideao@ccc.ufcg.edu.br>
2022-03-27 20:57:22 -0300

    admin update

--- app/urls.py
+++ app/urls.py
@@ -29,7 +29,26 @@ urlpatterns = [
 
 
     #admin urls
-    path('administrador/', views.admin.as_view(), name = 'admin'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('acchat/', views.ACreateChat.as_view(), name='acchat'),
+    path('alchat/', views.AListChat.as_view(), name='alchat'),
+    path('aabook_form/', views.aabook_form, name='aabook_form'),
+    path('aabook/', views.aabook, name='aabook'),
+    path('albook/', views.ABookListView.as_view(), name='albook'),
+    path('ambook/', views.AManageBook.as_view(), name='ambook'),
+    path('adbook/<int:pk>', views.ADeleteBook.as_view(), name='adbook'),
+    path('avbook/<int:pk>', views.AViewBook.as_view(), name='avbook'),
+    path('aebook/<int:pk>', views.AEditView.as_view(), name='aebook'),
+    path('adrequest/', views.ADeleteRequest.as_view(), name='adrequest'),
+    path('afeedback/', views.AFeedback.as_view(), name='afeedback'),
+    path('asearch/', views.asearch, name='asearch'),
+    path('adbookk/<int:pk>', views.ADeleteBookk.as_view(), name='adbookk'),
+    path('create_user_form/', views.create_user_form, name='create_user_form'),
+    path('aluser/', views.ListUserView.as_view(), name='aluser'),
+    path('create_use/', views.create_user, name='create_user'),
+    path('alvuser/<int:pk>', views.ALViewUser.as_view(), name='alvuser'),
+    path('aeuser/<int:pk>', views.AEditUser.as_view(), name='aeuser'),
+    path('aduser/<int:pk>', views.ADeleteUser.as_view(), name='aduser'),
 
 
 
--- app/views.py
+++ app/views.py
@@ -21,15 +21,19 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 
+
 def login_form(request):
 	return render(request, 'login.html')
+
 
 def logoutView(request):
 	logout(request)
 	return redirect('home')
 
+
 def register_form(request):
 	return render(request, 'register.html')
+
 
 def registerView(request):
 	if request.method == 'POST':
@@ -47,8 +51,8 @@ def registerView(request):
 		return redirect('regform')
 
 
+def loginview(request):
 
-def loginView(request):
 	if request.method == 'POST':
 		username = request.POST['username']
 		password = request.POST['password']
@@ -69,6 +73,261 @@ def loginView(request):
 #Admin views
 class admin(TemplateView):
 	template_name = 'admin/home.html'
+
+
+def admin(request):
+	book = models.Book.objects.all().count()
+	user = models.User.objects.all().count()
+
+	context = {'book': book, 'user': user}
+
+	return render(request, 'admin/home.html', context)
+
+
+def create_user_form(request):
+	choice = ['1', '0', 'Publisher', 'Admin', 'Librarian']
+	choice = {'choice': choice}
+
+	return render(request, 'admin/add_user.html', choice)
+
+
+class ADeleteUser(SuccessMessageMixin, DeleteView):
+	model = models.User
+	template_name = 'admin/confirm_delete3.html'
+	success_url = reverse_lazy('aluser')
+	success_message = "Data successfully deleted"
+
+
+class AEditUser(SuccessMessageMixin, UpdateView):
+	model = models.User
+	form_class = forms.UserForm
+	template_name = 'admin/edit_user.html'
+	success_url = reverse_lazy('aluser')
+	success_message = "Data successfully updated"
+
+
+class ListUserView(generic.ListView):
+	model = models.User
+	template_name = 'admin/list_users.html'
+	context_object_name = 'users'
+	paginate_by = 4
+
+	def get_queryset(self):
+		return models.User.objects.order_by('-id')
+
+
+def create_user(request):
+	choice = ['1', '0', 'Publisher', 'Admin', 'Librarian']
+	choice = {'choice': choice}
+	if request.method == 'POST':
+		first_name = request.POST['first_name']
+		last_name = request.POST['last_name']
+		username = request.POST['username']
+		userType = request.POST['userType']
+		email = request.POST['email']
+		password = request.POST['password']
+		password = make_password(password)
+		print("User Type")
+		print(userType)
+		if userType == "Publisher":
+			a = models.User(first_name=first_name, last_name=last_name, username=username, email=email, password=password,
+					 is_publisher=True)
+			a.save()
+			messages.success(request, 'Member was created successfully!')
+			return redirect('aluser')
+		elif userType == "Admin":
+			a = models.User(first_name=first_name, last_name=last_name, username=username, email=email, password=password,
+					 is_admin=True)
+			a.save()
+			messages.success(request, 'Member was created successfully!')
+			return redirect('aluser')
+		elif userType == "Librarian":
+			a = models.User(first_name=first_name, last_name=last_name, username=username, email=email, password=password,
+					 is_librarian=True)
+			a.save()
+			messages.success(request, 'Member was created successfully!')
+			return redirect('aluser')
+		else:
+			messages.success(request, 'Member was not created')
+			return redirect('create_user_form')
+	else:
+		return redirect('create_user_form')
+
+
+class ALViewUser(DetailView):
+	model = models.User
+	template_name = 'admin/user_detail.html'
+
+
+class ACreateChat(LoginRequiredMixin, CreateView):
+	form_class = ChatForm
+	model = models.Chat
+	template_name = 'admin/chat_form.html'
+	success_url = reverse_lazy('alchat')
+
+	def form_valid(self, form):
+		self.object = form.save(commit=False)
+		self.object.user = self.request.user
+		self.object.save()
+		return super().form_valid(form)
+
+
+class AListChat(LoginRequiredMixin, ListView):
+	model = models.Chat
+	template_name = 'admin/chat_list.html'
+
+	def get_queryset(self):
+		return models.Chat.objects.filter(posted_at__lt=timezone.now()).order_by('posted_at')
+
+
+@login_required
+def aabook_form(request):
+	return render(request, 'admin/add_book.html')
+
+
+@login_required
+def aabook(request):
+	if request.method == 'POST':
+		title = request.POST['title']
+		author = request.POST['author']
+		year = request.POST['year']
+		publisher = request.POST['publisher']
+		desc = request.POST['desc']
+		cover = request.FILES['cover']
+		pdf = request.FILES['pdf']
+		current_user = request.user
+		user_id = current_user.id
+		username = current_user.username
+
+		a = models.Book(title=title, author=author, year=year, publisher=publisher,
+				 desc=desc, cover=cover, pdf=pdf, uploaded_by=username, user_id=user_id)
+		a.save()
+		messages.success(request, 'Book was uploaded successfully')
+		return redirect('albook')
+	else:
+		messages.error(request, 'Book was not uploaded successfully')
+		return redirect('aabook_form')
+
+
+class ABookListView(LoginRequiredMixin, ListView):
+	model = models.Book
+	template_name = 'admin/book_list.html'
+	context_object_name = 'books'
+	paginate_by = 3
+
+	def get_queryset(self):
+		return models.Book.objects.order_by('-id')
+
+
+class AManageBook(LoginRequiredMixin, ListView):
+	model = models.Book
+	template_name = 'admin/manage_books.html'
+	context_object_name = 'books'
+	paginate_by = 3
+
+	def get_queryset(self):
+		return models.Book.objects.order_by('-id')
+
+
+class ADeleteBook(LoginRequiredMixin, DeleteView):
+	model = models.Book
+	template_name = 'admin/confirm_delete2.html'
+	success_url = reverse_lazy('ambook')
+	success_message = 'Data was dele successfully'
+
+
+class ADeleteBookk(LoginRequiredMixin, DeleteView):
+	model = models.Book
+	template_name = 'admin/confirm_delete.html'
+	success_url = reverse_lazy('admin')
+	success_message = 'Data was dele successfully'
+
+
+class AViewBook(LoginRequiredMixin, DetailView):
+	model = models.Book
+	template_name = 'admin/book_detail.html'
+
+
+class AEditView(LoginRequiredMixin, UpdateView):
+	model = models.Book
+	form_class = forms.BookForm
+	template_name = 'admin/edit_book.html'
+	success_url = reverse_lazy('ambook')
+	success_message = 'Data was updated successfully'
+
+
+class ADeleteRequest(LoginRequiredMixin, ListView):
+	model = models.DeleteRequest
+	template_name = 'admin/delete_request.html'
+	context_object_name = 'feedbacks'
+	paginate_by = 3
+
+	def get_queryset(self):
+		return models.DeleteRequest.objects.order_by('-id')
+
+
+class AFeedback(LoginRequiredMixin, ListView):
+	model = models.Feedback
+	template_name = 'admin/feedback.html'
+	context_object_name = 'feedbacks'
+	paginate_by = 3
+
+	def get_queryset(self):
+		return models.Feedback.objects.order_by('-id')
+
+
+@login_required
+def asearch(request):
+	query = request.GET['query']
+	print(type(query))
+
+	# data = query.split()
+	data = query
+	print(len(data))
+	if (len(data) == 0):
+		return redirect('dashborad')
+	else:
+		a = data
+
+		# Searching for It
+		qs5 = models.Book.objects.filter(id__iexact=a).distinct()
+		qs6 = models.Book.objects.filter(id__exact=a).distinct()
+
+		qs7 = models.Book.objects.all().filter(id__contains=a)
+		qs8 = models.Book.objects.select_related().filter(id__contains=a).distinct()
+		qs9 = models.Book.objects.filter(id__startswith=a).distinct()
+		qs10 = models.Book.objects.filter(id__endswith=a).distinct()
+		qs11 = models.Book.objects.filter(id__istartswith=a).distinct()
+		qs12 = models.Book.objects.all().filter(id__icontains=a)
+		qs13 = models.Book.objects.filter(id__iendswith=a).distinct()
+
+		files = itertools.chain(qs5, qs6, qs7, qs8, qs9, qs10, qs11, qs12, qs13)
+
+		res = []
+		for i in files:
+			if i not in res:
+				res.append(i)
+
+		# word variable will be shown in html when user click on search button
+		word = "Searched Result :"
+		print("Result")
+
+		print(res)
+		files = res
+
+		page = request.GET.get('page', 1)
+		paginator = Paginator(files, 10)
+		try:
+			files = paginator.page(page)
+		except PageNotAnInteger:
+			files = paginator.page(1)
+		except EmptyPage:
+			files = paginator.page(paginator.num_pages)
+
+		if files:
+			return render(request, 'admin/result.html', {'files': files, 'word': word})
+		return render(request, 'admin/result.html', {'files': files, 'word': word})
+
 
 #Publisher views
 class publisher(TemplateView):
